78e3e265c1fafcb4956da67dde5377abbb051572
Rober <boblawyer1@gmail.com>
2022-08-02 20:35:50 -0300

    creadas las tarjetas de heroes

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
@@ -7,11 +8,13 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class ListadoComponent implements OnInit {
 
+  heroes: Heroe[] = [];
+
   constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
     this.heroesService.getHeroes()
-    .subscribe (resp => console.log(resp))
+    .subscribe (resp => this.heroes = resp)
   }
 
 }
