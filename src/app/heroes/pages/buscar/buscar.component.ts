b3eedf3df2e2269c58518fdaf4e380041d0b9c57
Rober <boblawyer1@gmail.com>
2022-08-03 17:20:20 -0300

    implementada la busqueda

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -6,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string = "";
+  heroes: Heroe[] = [];
+  heroeSeleccionado: Heroe | undefined;
+  
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+  }
+
+  buscando() {
+    this.heroesService.getSugerencias( this.termino.trim() )
+    .subscribe(heros => this.heroes = heros)
+  }
+
+  opcionSeleccionada( event: MatAutocompleteSelectedEvent) {
+    if(!event.option.value){
+      this.heroeSeleccionado = undefined
+      return
+    }
+    const heroe: Heroe = event.option.value
+    this.termino = heroe.superhero
+    this.heroesService.getHeroesPorId( heroe.id! )
+    .subscribe(heroe => this.heroeSeleccionado = heroe)
   }
 
 }
